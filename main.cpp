49c715bd504afb160888610237333af3ae6b5b2a
Eduardo <eduard.chernyak.00@gmail.com>
2021-11-27 21:04:17 +0700

    Генерация кода для всех 3х языков

--- main.cpp
+++ main.cpp
@@ -9,7 +9,18 @@ int main()
     ICodeFactory* icd = new JavaCodeFactory();
     CodeGenerator* cd = new CodeGenerator(icd);
     std::cout << cd->generateProgram() << std::endl;
+    delete cd;
+    ICodeFactory* icd2 = new CSharpCodeFactory();
+    cd = new CodeGenerator(icd2);
+    std::cout << cd->generateProgram() << std::endl;
+    delete cd;
+    ICodeFactory* icd3 = new CPPCodeFactory();
+    cd = new CodeGenerator(icd3);
+    std::cout << cd->generateProgram() << std::endl;
+
     delete icd;
     delete cd;
+    delete icd2;
+    delete icd3;
     return 0;
 }
